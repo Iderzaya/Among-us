bdabf3e4b1aba57b26e4771ffba1ffa585b1fe7b
Iderzaya <bh.iderzaya@gmail.com>
2022-03-04 16:58:34 +0800

    read Button done

--- pages/public/javascripts/components/readButton.js
+++ pages/public/javascripts/components/readButton.js
@@ -4,23 +4,62 @@ export default class ReadMore extends HTMLElement {
         this.innerRoot = this.attachShadow({
             mode: "open"
         });
-    }
 
-    connectedCallback() {
-        this.Render();
-    }
-
-    Render() {
+        this.showInfo = false;
         console.log("rendering...");
-        this.innerRoot.innerHTML =  `
+        this.innerRoot.innerHTML = `
             <style>
             button{
-                background-color: red;
+                display: inline-block;
+                text-align: center;
+                width: 200px;
+                border: rgba(255, 255, 255, .2);
+                border-style: solid;
+                border-width: 3px;
+                background-color: transparent;
+                padding: 10px 30px;
+                color: #fff;
+                text-decoration: none;
+                transition: all .3s ease-in-out
+            }
+            button:hover, button:focus{
+                color: #fff;
+                text-decoration: none;
+                bottom: 41%;
+                border: rgba(255, 0, 0, .4);
+                border-style: solid;
+                border-width: 3px
+            }
+            .info { 
+                display:none;
             }
             </style>
     
-            
-            <button>Read me</button>`;
+            <div class = "info">${this.getAttribute("details")}</div>
+
+            <button id = "toggle-info">Show Info</button>`;
+    }
+
+    connectedCallback() {
+        this.shadowRoot.querySelector("#toggle-info").addEventListener("click", () => this.toggleInfo());
+    }
+
+    toggleInfo() {
+        this.showInfo = !this.showInfo;
+
+        const info = this.shadowRoot.querySelector(".info");
+        const toggleBtn = this.shadowRoot.querySelector("#toggle-info");
+        if(this.showInfo == true){
+            info.style.display = "block";
+            toggleBtn.innerText = "Hide Info";
+        } else{
+            info.style.display = "none";
+            toggleBtn.innerText = "Show Info";
+        }
+    }
+
+    disconnectedCallback(){
+        this.shadowRoot.querySelector("#toggle-info").removeEventListener();
     }
 }
 
